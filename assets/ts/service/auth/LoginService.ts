8922687c50aa2526cb4a3de25b2dfbca09f82cf0
romain <romain.quentel.bzh29@gmail.com>
2022-10-14 18:59:26 +0200

    Fix header duplication after dependency upgrade

--- assets/ts/service/auth/LoginService.ts
+++ assets/ts/service/auth/LoginService.ts
@@ -68,12 +68,8 @@ export default class LoginService {
         grant_type: 'authorization_code',
         redirect_uri: location.href.replace(location.search, ''),
         code
-      })).toString(),
-      {
-        headers: {
-          'Content-type': 'application/x-www-form-urlencoded'
-        }
-      })
+      })).toString()
+    )
       .then((response) => {
         window.sessionStorage.setItem('access_token', response.data.access_token)
         window.sessionStorage.setItem('refresh_token', response.data.refresh_token)
